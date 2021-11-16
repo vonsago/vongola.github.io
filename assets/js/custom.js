4c619dfff1f85b9359f32a4ef21b253cba019546
Ars <ars.von.roseregen@gmail.com>
2021-11-16 16:50:33 +0800

    hugo first md

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?efa234dd0de80cabe5e7cb41e0f81e74";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+console.log('Message from assets/js/custom.js');
---
+++ content/en/posts/newpost.md
@@ -0,0 +1,6 @@
+---
+title: "Newpost"
+date: 2021-11-16T11:55:04+08:00
+draft: true
+---
+
