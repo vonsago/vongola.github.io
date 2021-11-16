7515fe784c22694a182f8c5df3fe0c0c1be06cb5
Ars <ars.von.roseregen@gmail.com>
2021-11-16 17:08:00 +0800

    up

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+
+#================================================================
+#   Copyright (C) 2021 Sangfor Ltd. All rights reserved.
+#   
+#   FileName: deploy.sh
+#   Author: Vassago
+#   CreateTime: 2021-11-16
+#   Description: Shell
+#
+#================================================================
+
+git subtree push --prefix public/* origin master
+
